2fb529cc9f98fa240ba7a2c26cc8d8d91cff1bb3
viniciusshenri96 <vinicius_henrique96@hotmail.com>
2022-06-02 12:27:35 -0300

    Adicionando Animação por tabs

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 'use strict';
 
 /* Animação Navegação por tabs */
+const ativo = 'ativo';
+function clickHandleTabs() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add(ativo);
+    function handleTabs(index) {
+      tabContent.forEach(item => {
+        item.classList.remove(ativo);
+      });
+      tabContent[index].classList.add(ativo);
+    }
+
+    tabMenu.forEach((item, index) => {
+      item.addEventListener('click', function () {
+        handleTabs(index);
+      });
+    });
+  }
+}
+clickHandleTabs();
